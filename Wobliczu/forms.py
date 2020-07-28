3c164ce3c9e29d18937c248e5f37381b113d6f49
Norbert <norbi2790@gmail.com>
2020-07-28 15:32:48 +0200

    Removed unnecessary fields of Article model.

--- Wobliczu/forms.py
+++ Wobliczu/forms.py
@@ -26,19 +26,12 @@ class AddArticleForm(forms.ModelForm):
     class Meta:
         model = Article
         fields = ('title', 'short_desc', 'content', 'miniature', 'main_image', 'main_tags', 'secondary_tags',
-                  'user', 'image_section_two', 'text_section_two', 'image_section_three', 'text_section_three',
-                  'image_section_four', 'text_section_four', 'slug', 'when_to_public')
+                  'user', 'slug', 'when_to_public')
         widgets = {
             'title': forms.Textarea(attrs={'class': 'input'}),
             'short_desc': forms.Textarea(attrs={'class': 'input'}),
             'miniature': forms.ClearableFileInput(attrs={'class': 'inputImage'}),
             'main_image': forms.ClearableFileInput(attrs={'class': 'inputImage'}),
-            'image_section_two': forms.ClearableFileInput(attrs={'class': 'inputImage'}),
-            'image_section_three': forms.ClearableFileInput(attrs={'class': 'inputImage'}),
-            'image_section_four': forms.ClearableFileInput(attrs={'class': 'inputImage'}),
-            'text_section_two': forms.Textarea(attrs={'class': 'input'}),
-            'text_section_three': forms.Textarea(attrs={'class': 'input'}),
-            'text_section_four': forms.Textarea(attrs={'class': 'input'}),
             'main_tags': forms.CheckboxSelectMultiple(attrs={'class': 'checkboxMltp'}),
             'secondary_tags': forms.CheckboxSelectMultiple(attrs={'class': 'checkboxMltp'}),
             'when_to_public': forms.DateTimeInput(attrs={'class': 'input'}),
--- Wobliczu/models.py
+++ Wobliczu/models.py
@@ -57,14 +57,7 @@ class Article(models.Model):
     secondary_tags = models.ManyToManyField(SecondaryTags, null=True)
     miniature = models.ImageField(upload_to='media/miniatures/')
     main_image = models.ImageField(upload_to='media/main_images/')
-    text_section_two = models.CharField(max_length=10000, null=True, blank=True)
-    image_section_two = models.ImageField(upload_to='media/sect2/', null=True, blank=True)
-    text_section_three = models.CharField(max_length=10000, null=True, blank=True,)
-    image_section_three = models.ImageField(upload_to='media/sect3/', null=True, blank=True)
-    text_section_four = models.CharField(max_length=10000, null=True, blank=True)
-    image_section_four = models.ImageField(upload_to='media/sect4/', null=True, blank=True)
     when_to_public = models.DateTimeField(default=timezone.now, blank=True)
-    #shown_tag = models.ForeignKey(SecondaryTags, on_delete=models.CASCADE, related_name='%(class)s_requests_created', null=True, blank=True)
 
     def delete(self, **kwargs):
         print('Usuwam artykuł id: ', self.ID, ' o tytule: ', self.title)
