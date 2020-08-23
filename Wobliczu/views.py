351bfc598650afcf14f424510a3374a6bb091823
MokrySzczupak <norbi2790@gmail.com>
2020-08-23 12:22:13 +0200

    Changed error_404_view function to return status 404

--- Wobliczu/views.py
+++ Wobliczu/views.py
@@ -19,7 +19,7 @@ from django.views.generic.list import ListView
 
 
 def error_404_view(request, exception):
-    return render(request, '404.html')
+    return render(request, '404.html', status=404)
 
 
 def renderHome(request):
